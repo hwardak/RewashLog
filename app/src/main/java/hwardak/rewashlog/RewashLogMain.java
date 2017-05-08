01ca51dafbbed759be898dfad4826e41ef5a43df
HWardak <h.wardak@hotmail.com>
2017-05-08 19:40:48 -0400

    Completed XML file, started Main activity, and added employeeList.java

--- app/src/main/java/hwardak/rewashlog/RewashLogMain.java
+++ app/src/main/java/hwardak/rewashlog/RewashLogMain.java
@@ -2,12 +2,99 @@ package hwardak.rewashlog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class RewashLogMain extends AppCompatActivity {
+
+    ScrollView mainScrollView;
+
+    private LinearLayout nameLinearLayout;
+    private LinearLayout timeLinearLayout;
+    private LinearLayout dateLinearLayout;
+    private LinearLayout washPackageLinearLayout;
+    private LinearLayout reasonLinearLayout;
+    private LinearLayout instructionsLinearLayout;
+
+    private TextView instructionsTextView;
+    private EditText employeeIdEditText;
+    private EditText employeeNameEditText;
+    private EditText timeEditText;
+    private EditText dateEditText;
+
+    private RadioGroup washPackageRadioGroup;
+    private RadioButton quickRadioButton;
+    private RadioButton fullRadioButton;
+    private RadioButton luxuryRadioButton;
+
+    private CheckBox notCleanCheckbox;
+    private CheckBox noSoapCheckbox;
+    private CheckBox leftOverSoapCheckbox;
+    private CheckBox notDryCheckbox;
+    private CheckBox expiredCodeCheckbox;
+    private CheckBox customerSatisfactionCheckbox;
+    private CheckBox testWashCheckbox;
+
+    private Button saveButton;
+    private Button instructionsOkButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewash_log_main);
+
+        mainScrollView = (ScrollView) findViewById(R.id.mainScrollView);
+
+        nameLinearLayout = (LinearLayout) findViewById(R.id.nameLinearLayout);
+        timeLinearLayout = (LinearLayout) findViewById(R.id.timeLinearLayout);
+        dateLinearLayout = (LinearLayout) findViewById(R.id.dateLinearLayout);
+        washPackageLinearLayout = (LinearLayout) findViewById(R.id.washPackageLinearLayout);
+        reasonLinearLayout = (LinearLayout) findViewById(R.id.reasonLinearLayout);
+        instructionsLinearLayout = (LinearLayout) findViewById(R.id.instructionsLinearLayout);
+
+        instructionsTextView = (TextView) findViewById(R.id.instructionsTextView);
+        employeeIdEditText = (EditText) findViewById(R.id.employeeIdEditText);
+        employeeNameEditText = (EditText) findViewById(R.id.employeeNameEditText);
+        timeEditText = (EditText) findViewById(R.id.timeEditText);
+        dateEditText = (EditText) findViewById(R.id.dateEditText);
+
+        washPackageRadioGroup = (RadioGroup) findViewById(R.id.washPackageRadioGroup);
+        quickRadioButton = (RadioButton) findViewById(R.id.quickRadioButton);
+        fullRadioButton = (RadioButton) findViewById(R.id.fullRadioButton);
+        luxuryRadioButton = (RadioButton) findViewById(R.id.luxuryRadioButton);
+
+        notCleanCheckbox = (CheckBox) findViewById(R.id.notCleanCheckbox);
+        noSoapCheckbox = (CheckBox) findViewById(R.id.noSoapCheckbox);
+        leftOverSoapCheckbox = (CheckBox) findViewById(R.id.leftOverSoapCheckbox);
+        notDryCheckbox = (CheckBox) findViewById(R.id.notDryCheckbox);
+        expiredCodeCheckbox = (CheckBox) findViewById(R.id.expiredCodeCheckbox);
+        customerSatisfactionCheckbox = (CheckBox) findViewById(R.id.customerSatisfactionCheckbox);
+        testWashCheckbox = (CheckBox) findViewById(R.id.testWashCheckbox);
+
+        saveButton = (Button) findViewById(R.id.saveButton);
+        instructionsOkButton = (Button) findViewById(R.id.instructionsOkButton);
+
+        hideAllLayouts();
     }
+
+    private void hideAllLayouts() {
+        nameLinearLayout.setVisibility(View.INVISIBLE);
+        timeLinearLayout.setVisibility(View.INVISIBLE);
+        dateLinearLayout.setVisibility(View.INVISIBLE);
+        washPackageLinearLayout.setVisibility(View.INVISIBLE);
+        reasonLinearLayout.setVisibility(View.INVISIBLE);
+        instructionsLinearLayout.setVisibility(View.INVISIBLE);
+    }
+
+
 }
---
+++ app/src/main/java/hwardak/rewashlog/employeeList.java
@@ -0,0 +1,8 @@
+package hwardak.rewashlog;
+
+/**
+ * Created by HWardak on 2017-05-08.
+ */
+
+public final class employeeList {
+}
