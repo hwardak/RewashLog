83a49cec755c0d14e74d6ccdb908604efd954ca5
HWardak <h.wardak@hotmail.com>
2017-05-08 04:07:27 -0400

    Initial commit of XML layout.

---
+++ app/src/main/java/hwardak/rewashlog/RewashLogMain.java
@@ -0,0 +1,13 @@
+package hwardak.rewashlog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RewashLogMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewash_log_main);
+    }
+}
